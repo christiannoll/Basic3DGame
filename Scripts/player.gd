bbf2cd2a79c5f03ca1e0afb9d24607e6b4c6ac7e
Christian Noll <christiannoll@gmx.net>
2026-02-22 17:42:54 +0100

    Added new script 'beginning_cutscene'

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,21 +8,23 @@ const JUMP_VELOCITY = 4.5
 var sprint_slider
 var sprint_drain_amount = 0.1
 var sprint_refresh_amount = 0.1
+var movable = false
 
 func _ready() -> void:
 	ORIGINAL_SPEED = SPEED
 	sprint_slider = get_node("/root/" + get_tree().current_scene.name + "/UI/sprint_slider")
 
 func _process(delta: float) -> void:
-	if SPEED == SPRINT_SPEED:
-		sprint_slider.value = sprint_slider.value - sprint_drain_amount * delta
-		if sprint_slider.value == sprint_slider.min_value:
-			SPEED = ORIGINAL_SPEED
-	if SPEED != SPRINT_SPEED:
-		if sprint_slider.value < sprint_slider.max_value:
-			sprint_slider.value = sprint_slider.value + sprint_refresh_amount * delta
-		if sprint_slider.value == sprint_slider.max_value:
-			sprint_slider.visible = false
+	if movable == true:
+		if SPEED == SPRINT_SPEED:
+			sprint_slider.value = sprint_slider.value - sprint_drain_amount * delta
+			if sprint_slider.value == sprint_slider.min_value:
+				SPEED = ORIGINAL_SPEED
+		if SPEED != SPRINT_SPEED:
+			if sprint_slider.value < sprint_slider.max_value:
+				sprint_slider.value = sprint_slider.value + sprint_refresh_amount * delta
+			if sprint_slider.value == sprint_slider.max_value:
+				sprint_slider.visible = false
 	
 
 func _physics_process(delta: float) -> void:
@@ -30,24 +32,25 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if movable == true:
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-		if Input.is_action_just_pressed("sprint"):
-			sprint_slider.visible = true
-			SPEED = SPRINT_SPEED
-		if Input.is_action_just_released("sprint"):
-			SPEED = ORIGINAL_SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var input_dir := Input.get_vector("left", "right", "forward", "backward")
+		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+			if Input.is_action_just_pressed("sprint"):
+				sprint_slider.visible = true
+				SPEED = SPRINT_SPEED
+			if Input.is_action_just_released("sprint"):
+				SPEED = ORIGINAL_SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
