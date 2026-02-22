3cebc8e7a6d0e0b5c95d02c837a693a4a6e1682e
Christian Noll <christiannoll@gmx.net>
2026-02-22 17:56:35 +0100

    Avoid showing sprint bar in beginning cut scene

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -15,16 +15,15 @@ func _ready() -> void:
 	sprint_slider = get_node("/root/" + get_tree().current_scene.name + "/UI/sprint_slider")
 
 func _process(delta: float) -> void:
-	if movable == true:
-		if SPEED == SPRINT_SPEED:
-			sprint_slider.value = sprint_slider.value - sprint_drain_amount * delta
-			if sprint_slider.value == sprint_slider.min_value:
-				SPEED = ORIGINAL_SPEED
-		if SPEED != SPRINT_SPEED:
-			if sprint_slider.value < sprint_slider.max_value:
-				sprint_slider.value = sprint_slider.value + sprint_refresh_amount * delta
-			if sprint_slider.value == sprint_slider.max_value:
-				sprint_slider.visible = false
+	if SPEED == SPRINT_SPEED:
+		sprint_slider.value = sprint_slider.value - sprint_drain_amount * delta
+		if sprint_slider.value == sprint_slider.min_value:
+			SPEED = ORIGINAL_SPEED
+	if SPEED != SPRINT_SPEED:
+		if sprint_slider.value < sprint_slider.max_value:
+			sprint_slider.value = sprint_slider.value + sprint_refresh_amount * delta
+		if sprint_slider.value == sprint_slider.max_value:
+			sprint_slider.visible = false
 	
 
 func _physics_process(delta: float) -> void:
