588c6013065d630c0ebcfbf932219e469d58086f
Christian Noll <christiannoll@gmx.net>
2026-02-24 16:02:34 +0100

    Fix camera control during beginning cutscene

--- Scripts/beginning_cutscene.gd
+++ Scripts/beginning_cutscene.gd
@@ -8,5 +8,6 @@ func _ready() -> void:
 	await get_tree().create_timer(8.0, false).timeout
 	get_node("/root/" + get_tree().current_scene.name + "/Player").movable = true
 	get_node("/root/" + get_tree().current_scene.name + "/Player/head/Camera3D").current = true
+	get_node("/root/" + get_tree().current_scene.name + "/Player/head/").movable = true
 	$cutscene_cam.current = false
 	queue_free()
--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 var sens = 0.005
+var movable = false
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -8,7 +9,7 @@ func _ready():
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion && movable == true:
 		get_parent().rotate_y(event.relative.x * sens)
 		rotate_x(event.relative.y * sens)
 		rotation.x = clamp(rotation.x, deg_to_rad(-90), deg_to_rad(90))	
