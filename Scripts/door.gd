6da35ed3da5f0bf9fdaee0f26238294b7a021d6c
Christian Noll <christiannoll@gmx.net>
2026-03-17 17:46:32 +0100

    Updated door script to play door sounds

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -10,12 +10,15 @@ func interact():
 		interactable = false
 		opened = !opened
 		if opened == false:
+			$close.play()
 			$AnimationPlayer.play("close")
 		else:
+			$open.play()
 			$AnimationPlayer.play("open")
 		await get_tree().create_timer(1.0, false).timeout
 		interactable = true
 	if interactable == true && get_parent().get_parent().locked == true:
+		$locked.play()
 		interactable = false
 		$AnimationPlayer.play("locked")
 		await get_tree().create_timer(0.7, false).timeout
