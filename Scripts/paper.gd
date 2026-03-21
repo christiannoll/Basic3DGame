7762bdd544e21e2b64dc36523ca047a58f9e4d5f
Christian Noll <christiannoll@gmx.net>
2026-03-21 14:33:16 +0100

    Updated paper script

--- Scripts/paper.gd
+++ Scripts/paper.gd
@@ -1,6 +1,7 @@
 extends StaticBody3D
 
 @export var paper_material: StandardMaterial3D
+@export var paper_ui_texture: Texture2D
 var toggle = false
 
 func _ready() -> void:
@@ -8,6 +9,7 @@ func _ready() -> void:
 	
 func interact():
 	toggle = !toggle
+	get_node("/root/" + get_tree().current_scene.name + "/UI/paper").texture = paper_ui_texture
 	get_node("/root/" + get_tree().current_scene.name + "/UI/paper").visible = toggle
 	get_node("/root/" + get_tree().current_scene.name + "/Player").movable = !toggle
 	get_node("/root/" + get_tree().current_scene.name + "/Player/head").movable = !toggle
