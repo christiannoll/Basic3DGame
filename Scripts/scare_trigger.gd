66615d9237366cf052115e3a5ec3c0626dbaa3ae
Christian Noll <christiannoll@gmx.net>
2026-03-28 11:50:30 +0100

    Updated scare_trigger script

--- Scripts/scare_trigger.gd
+++ Scripts/scare_trigger.gd
@@ -1,1 +1,9 @@
 extends Area3D
+
+@export var animationPlayer: AnimationPlayer
+@export var anim_name: String
+	
+func trigger_entered(body):
+	if body == get_node("/root/" + get_tree().current_scene.name + "/Player"):
+		animationPlayer.play(anim_name)
+		monitoring = false
