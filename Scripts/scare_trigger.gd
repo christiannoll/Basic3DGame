112968e2accb8e843dda0cbaba57e59eb3841c61
Christian Noll <christiannoll@gmx.net>
2026-03-28 12:23:36 +0100

    Ensure scare trigger only released once

--- Scripts/scare_trigger.gd
+++ Scripts/scare_trigger.gd
@@ -2,8 +2,9 @@ extends Area3D
 
 @export var animationPlayer: AnimationPlayer
 @export var anim_name: String
+var token = 0
 	
 func trigger_entered(body):
-	if body == get_node("/root/" + get_tree().current_scene.name + "/Player"):
+	if body == get_node("/root/" + get_tree().current_scene.name + "/Player") && token == 0:
 		animationPlayer.play(anim_name)
-		monitoring = false
+		token = 1
