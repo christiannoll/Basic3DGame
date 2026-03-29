cf029dd8ec1711dd7d2b2237ae49e3ae9ba0f5cf
Christian Noll <christiannoll@gmx.net>
2026-03-29 13:14:17 +0200

    Added scare sound to scare triggers

--- Scripts/scare_trigger.gd
+++ Scripts/scare_trigger.gd
@@ -2,9 +2,12 @@ extends Area3D
 
 @export var animationPlayer: AnimationPlayer
 @export var anim_name: String
+@export var scare_sound: AudioStreamPlayer
 var token = 0
 	
 func trigger_entered(body):
 	if body == get_node("/root/" + get_tree().current_scene.name + "/Player") && token == 0:
 		animationPlayer.play(anim_name)
+		if scare_sound != null:
+			scare_sound.play()
 		token = 0 #Todo: find another solution here
