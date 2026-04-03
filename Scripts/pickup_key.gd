c399353f8874d3c498aac57585e9caa89413b07b
Christian Noll <christiannoll@gmx.net>
2026-04-03 15:14:19 +0200

    Adopted scripts to reparenting nodes in hierarchy

--- Scripts/pickup_key.gd
+++ Scripts/pickup_key.gd
@@ -2,5 +2,5 @@ extends StaticBody3D
 
 
 func interact():
-	get_node("/root/" + get_tree().current_scene.name + "/keys_pickup").play()
+	get_node("/root/" + get_tree().current_scene.name + "/Sounds/keys_pickup").play()
 	queue_free()
