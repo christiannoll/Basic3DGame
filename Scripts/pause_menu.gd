e9b0d6e1e5e7fff604e9e3588cc89b7acd460991
Christian Noll <christiannoll@gmx.net>
2026-04-04 11:09:57 +0200

    Fixed an issue in pause_menu

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -16,6 +16,7 @@ func resume():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func back_to_menu():
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 	
 func quit_game():
