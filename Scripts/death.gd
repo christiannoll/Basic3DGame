f6b7f8f4b270a3577af96e1d69934bbd609eee88
Christian Noll <christiannoll@gmx.net>
2026-04-08 06:58:56 +0200

    Added death scene

---
+++ Scripts/death.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@export var scene_name: String
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	await get_tree().create_timer(4.0, false).timeout
+	get_tree().current_scene_to_file("res://Scenes/" + scene_name + ".tscn")
