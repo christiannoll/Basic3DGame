c17ec7fcf3baf5462a51a14aa30afb180ce243e3
Christian Noll <christiannoll@gmx.net>
2026-04-11 20:48:02 +0200

    Fixed an issue in death script

--- Scripts/death.gd
+++ Scripts/death.gd
@@ -5,4 +5,4 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await get_tree().create_timer(4.0, false).timeout
-	get_tree().current_scene_to_file("res://Scenes/" + scene_name + ".tscn")
+	get_tree().change_scene_to_file("res://Scenes/" + scene_name + ".tscn")
