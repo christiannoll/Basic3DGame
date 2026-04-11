b5dd1a7281e3dc9e8aeb7fe686b0f2befd5370ec
Christian Noll <christiannoll@gmx.net>
2026-04-11 15:40:27 +0200

    Fixed issues in monster script

--- Scripts/level_manager.gd
+++ Scripts/level_manager.gd
@@ -6,4 +6,4 @@ func _ready() -> void:
 	player = get_node("/root/" + get_tree().current_scene.name + "/Player")
 
 func _process(delta: float) -> void:
-	get_tree().call_group("monster", "update_target_location", player.global_transform_origin)
+	get_tree().call_group("monster", "update_target_location", player.global_transform.origin)
--- Scripts/monster.gd
+++ Scripts/monster.gd
@@ -14,10 +14,10 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if visible:
 		if not is_on_floor():
-			velocity -= gravity * delta
+			velocity.y -= gravity * delta
 		var current_location = global_transform.origin
 		var next_location = $NavigationAgent3D.get_next_path_position()
-		var new_velocity = (next_location - current_location).normalized * SPEED
+		var new_velocity = (next_location - current_location).normalized() * SPEED
 		$NavigationAgent3D.set_velocity(new_velocity)
 		var look_dir = atan2(-velocity.y, -velocity.z)
 		rotation.y = look_dir
