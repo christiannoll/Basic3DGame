3b67134c3804a75b330abd27d8ff614a8e766c5c
Christian Noll <christiannoll@gmx.net>
2026-04-14 10:29:35 +0200

    Added func spawn_monster

--- Scripts/monster_spawner.gd
+++ Scripts/monster_spawner.gd
@@ -1,11 +1,7 @@
 extends Area3D
 
+@export var monster: CharacterBody3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func spawn_monster(body):
+	if body == get_node("/root/" + get_tree().current_scene.name + "/Player"):
+		monster.visible = true
