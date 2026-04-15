5a2dce24a78eda4930b6844d7def927e0c050eef
Christian Noll <christiannoll@gmx.net>
2026-04-15 09:35:31 +0200

    Added ready func in flashlight script

--- Scripts/flashlight.gd
+++ Scripts/flashlight.gd
@@ -1,6 +1,11 @@
 extends SpotLight3D
 
 var picked_up = false
+var flashlight_ui
+var flashlight_energy
+
+func _ready() -> void:
+	flashlight_ui = get_node("/root/" + get_tree().current_scene.name + "/UI/flashlight_stuff")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
