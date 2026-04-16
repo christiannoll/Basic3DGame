d7ac036de5147877fd02ef10411ed5acae47375d
Christian Noll <christiannoll@gmx.net>
2026-04-16 11:57:52 +0200

    Check flashlight is picked up before interacting with the batteries

--- Scripts/battery.gd
+++ Scripts/battery.gd
@@ -2,11 +2,17 @@ extends StaticBody3D
 
 var flashlight_energy
 var energy_add_amount = 0.5
+var pickup_sound
+var flashlight
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pickup_sound = get_node("/root/" + get_tree().current_scene.name + "/Sounds/obj_pickup")
 	flashlight_energy = get_node("/root/" + get_tree().current_scene.name + "/UI/flashlight_stuff/flashlight_slider")
+	flashlight = get_node("/root/" + get_tree().current_scene.name + "/Player/head/flashlight")
 
 func interact() -> void:
-	flashlight_energy.value += energy_add_amount
-	queue_free()
+	if flashlight.picked_up == true:
+		flashlight_energy.value += energy_add_amount
+		pickup_sound.play()
+		queue_free()
