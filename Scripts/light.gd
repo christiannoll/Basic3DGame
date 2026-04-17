44432ec975db7712ff262956b735b34ef7b9780c
Christian Noll <christiannoll@gmx.net>
2026-04-17 13:52:38 +0200

    Fix some issues in the light script

--- Scripts/light.gd
+++ Scripts/light.gd
@@ -6,23 +6,26 @@ extends Node3D
 @export var min_flash_time: float
 @export var max_flash_time: float
 @export var loop_flashing: bool
+@export var animation_time: float
 var loop = true
 
 func _process(delta: float) -> void:
-	if loop && flash_lights_at_rand:
+	if loop == true && flash_lights_at_rand == true:
 		loop = false
 		var rng = RandomNumberGenerator.new()
 		var rand = rng.randf_range(min_time, max_time)
 		await get_tree().create_timer(rand, false).timeout
-		if loop_flashing:
+		if loop_flashing == true:
 			$AnimationPlayer.get_animation("flashing_light").loop = true
 		$AnimationPlayer.play("flashing_light")
-		if loop_flashing:
+		if loop_flashing == false:
+			await get_tree().create_timer(animation_time, false).timeout
+			$AnimationPlayer.play("RESET")
+			loop = true
+		if loop_flashing == true:
 			var rand2 = rng.randf_range(min_flash_time, max_flash_time)
 			await get_tree().create_timer(rand2, false).timeout
 			$AnimationPlayer.play("RESET")
-			loop = true
-		else:
 			loop = true
 			
 			
