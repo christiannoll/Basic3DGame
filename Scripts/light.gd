828a057490dcb49a3cc9f8bea7464dac65e16695
Christian Noll <christiannoll@gmx.net>
2026-04-17 13:18:14 +0200

    Updated light script

--- Scripts/light.gd
+++ Scripts/light.gd
@@ -1,16 +1,29 @@
 extends Node3D
 
+@export var flash_lights_at_rand: bool
 @export var min_time: float
 @export var max_time: float
 @export var min_flash_time: float
 @export var max_flash_time: float
+@export var loop_flashing: bool
 var loop = true
 
 func _process(delta: float) -> void:
-	if loop:
+	if loop && flash_lights_at_rand:
 		loop = false
 		var rng = RandomNumberGenerator.new()
 		var rand = rng.randf_range(min_time, max_time)
 		await get_tree().create_timer(rand, false).timeout
-		pass
+		if loop_flashing:
+			$AnimationPlayer.get_animation("flashing_light").loop = true
+		$AnimationPlayer.play("flashing_light")
+		if loop_flashing:
+			var rand2 = rng.randf_range(min_flash_time, max_flash_time)
+			await get_tree().create_timer(rand2, false).timeout
+			$AnimationPlayer.get_animation("flashing_light").loop = false
+			loop = true
+		else:
+			loop = true
+			
+			
 		
