6e6965dfd09524a6bba3296ec43ddb32324fea65
Christian Noll <christiannoll@gmx.net>
2026-04-18 11:23:54 +0200

    Added enemy_raycast script

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -3,6 +3,15 @@ extends StaticBody3D
 var interactable = true
 var opened = false
 
+func enemy_interact():
+	if opened == false && get_parent().get_parent().locked == false:
+		opened = true
+		$open.play()
+		interactable = false
+		$AnimationPlayer.play("open")
+		await get_tree().create_timer(1.0, false).timeout
+		interactable = true
+
 func interact():
 	if get_parent().get_parent().locked == true && get_parent().get_parent().key == null:
 		get_parent().get_parent().locked = false
