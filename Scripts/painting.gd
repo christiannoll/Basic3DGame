9107f65af212c5d6bd3d870fa89210d1254be65c
Christian Noll <christiannoll@gmx.net>
2026-04-21 08:41:43 +0200

    Implemented paintaing script

--- Scripts/painting.gd
+++ Scripts/painting.gd
@@ -1,11 +1,6 @@
 extends StaticBody3D
 
+@export var painting_mat: StandardMaterial3D
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	$MeshInstance3D2.material_override = painting_mat
