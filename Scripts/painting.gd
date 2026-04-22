f290d43e124c37b15b6668d77139dfaa7ccc5686
Christian Noll <christiannoll@gmx.net>
2026-04-22 09:46:30 +0200

    Added scare func in painting script

--- Scripts/painting.gd
+++ Scripts/painting.gd
@@ -1,6 +1,18 @@
 extends StaticBody3D
 
 @export var painting_mat: StandardMaterial3D
+@export var scary_painting: StandardMaterial3D
+@export var stare_time: float
+@export var scare_time: float
 
 func _ready() -> void:
 	$MeshInstance3D2.material_override = painting_mat
+	
+func scare():
+	await get_tree().create_timer(stare_time, false).timeout
+	$MeshInstance3D2.material_override = scary_painting
+	await get_tree().create_timer(scare_time, false).timeout
+	$MeshInstance3D2.material_override = painting_mat
+
+func stop_scare():
+	pass
