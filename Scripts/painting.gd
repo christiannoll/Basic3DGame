3419a26cdf83bb1bfa81e647e57d5be1779ae451
Christian Noll <christiannoll@gmx.net>
2026-04-22 10:22:16 +0200

    Updated painting script

--- Scripts/painting.gd
+++ Scripts/painting.gd
@@ -4,17 +4,21 @@ extends StaticBody3D
 @export var scary_painting: StandardMaterial3D
 @export var stare_time: float
 @export var scare_time: float
+var looking = false
 
 func _ready() -> void:
 	$MeshInstance3D2.material_override = painting_mat
 	
 func scare():
-	await get_tree().create_timer(stare_time, false).timeout
-	$jumpscare.play()
-	$MeshInstance3D2.material_override = scary_painting
-	await get_tree().create_timer(scare_time, false).timeout
-	$jumpscare.stop()
-	$MeshInstance3D2.material_override = painting_mat
+	if looking == false:
+		looking = true
+		await get_tree().create_timer(stare_time, false).timeout
+		if looking == true:
+			$jumpscare.play()
+			$MeshInstance3D2.material_override = scary_painting
+			await get_tree().create_timer(scare_time, false).timeout
+			$jumpscare.stop()
+			$MeshInstance3D2.material_override = painting_mat
 
 func stop_scare():
 	pass
