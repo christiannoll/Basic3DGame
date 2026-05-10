c36932b88321b6e1919857bbdacc5d725536c28d
Christian Noll <christiannoll@gmx.net>
2026-05-10 09:54:45 +0200

    Adjust function toggle_switch

--- Scripts/light_switch.gd
+++ Scripts/light_switch.gd
@@ -4,11 +4,17 @@ extends StaticBody3D
 @export var light_off: StandardMaterial3D
 @export var light_on: StandardMaterial3D
 var toggle = false
+var interactable = true
 
 func toggle_switch():
-	toggle = !toggle
-	if toggle == false:
-		light.get_node("MeshInstance3D").material_override = light_off
-	else:
-		light.get_node("MeshInstance3D").material_override = light_on
-	light.get_node("OmniLight3D").visible = toggle
+	if interactable:
+		interactable = false
+		$AnimationPlayer.play("press_button")
+		toggle = !toggle
+		if toggle == false:
+			light.get_node("MeshInstance3D").material_override = light_off
+		else:
+			light.get_node("MeshInstance3D").material_override = light_on
+		light.get_node("OmniLight3D").visible = toggle
+		await get_tree().create_timer(0.6, false).timeout
+		interactable = true
