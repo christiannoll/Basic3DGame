1eb9298a1ca7d09dbe653b0e50fd84b6245c8d97
Christian Noll <christiannoll@gmx.net>
2026-05-13 18:00:26 +0200

    Update object script

--- Scripts/object.gd
+++ Scripts/object.gd
@@ -1,7 +1,13 @@
 extends StaticBody3D
 
+var object_counter
+
+func _ready() -> void:
+	object_counter = get_tree().current_scene.get_node("UI/object_counter")
+
 func interact():
 	get_parent().num_of_objects += 1
+	object_counter.text = str(get_parent().num_of_objects) + "/3"
 	if get_parent().num_of_objects >= 3:
 		pass
 	queue_free()
