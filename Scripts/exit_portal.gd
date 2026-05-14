e03226371379bfc4ed4c8c46c54fa255c8398caf
Christian Noll <christiannoll@gmx.net>
2026-05-14 10:33:13 +0200

    Updated exit_portal script

--- Scripts/exit_portal.gd
+++ Scripts/exit_portal.gd
@@ -1,11 +1,7 @@
 extends Area3D
 
+@export var scene_name: String
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func enter_trigger(body):
+	if body.name == "Player":
+		get_tree().change_scene_to_file("res://Scenes/" + scene_name + ".tscn")		
